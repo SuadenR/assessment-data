b414e28e4efdf140fd8858641f6a69999c8c4b91
Suaden.Rowland <suadenrowland@gmail.com>
2023-10-24 08:10:37 -0700

    fixed link

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const CONNECTION_STRING = require('postgres:///unit5')
 const SERVER_PORT = 4004
 const {seed, getCountries, getCities, createCity, deleteCity} = require('./controller.js')
 
